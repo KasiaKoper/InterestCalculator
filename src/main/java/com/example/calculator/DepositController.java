b500e0f16f5a330ec77ca265051836b5768702c9
KasiaKoper <katarzynakoper2@gmail.com>
2019-07-29 19:32:04 +0200

    controller added

--- src/main/java/com/example/calculator/DepositController.java
+++ src/main/java/com/example/calculator/DepositController.java
@@ -1,9 +1,18 @@
 package com.example.calculator;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
 
 @Controller
 public class DepositController {
 
 
+    @GetMapping("/interest")
+    public String calculateDeposit(ModelMap modelMap) {
+        modelMap.put("employee", new Deposit());
+        return "home";
+    }
 }
