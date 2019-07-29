52d24369334812b4f09262d4ac89b7e55290f306
KasiaKoper <katarzynakoper2@gmail.com>
2019-07-29 18:33:47 +0200

    first commit

---
+++ src/main/java/com/example/calculator/Deposit.java
@@ -0,0 +1,16 @@
+package com.example.calculator;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Deposit {
+
+
+    private int noOfYears;
+    private int  noOfMonths;
+    private double interestRate;
+    private String capitalisation;
+
+
+
+}
